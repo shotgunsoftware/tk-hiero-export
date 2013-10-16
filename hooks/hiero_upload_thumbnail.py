ae44cceff7064235fb4928cd85d29553a0b07f60
Rob Blau <rob@shotgunsoftware.com>
2013-10-16 09:06:02 -0700

    Include missing import.

--- hooks/hiero_upload_thumbnail.py
+++ hooks/hiero_upload_thumbnail.py
@@ -8,6 +8,7 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+import os
 import shutil
 import tempfile
 import traceback
