e0217d7b1ae91498b0ca5c4f4912fcab775b48e2
Alan Dann <alan.dann@shotgunsoftware.com>
2014-04-17 10:11:48 +0100

    For #25116 Renamed channel to output in Write Node placeholder metadata. This is to match the rename in the Shotgun Write Node itself. Closes #10

--- python/tk_hiero_export/sg_nuke_shot_export.py
+++ python/tk_hiero_export/sg_nuke_shot_export.py
@@ -169,8 +169,8 @@ class ShotgunNukeShotExporter(ShotgunHieroObjectBase, FnNukeShotExporter.NukeSho
         FnNukeShotExporter.NukeShotExporter._beforeNukeScriptWrite(self, script)
 
         for toolkit_specifier in self._preset.properties()["toolkitWriteNodes"]:
-            # break down a string like 'Toolkit Node: Mono Dpx ("editorial")' into name and channel
-            match = re.match("^Toolkit Node: (?P<name>.+) \(\"(?P<channel>.+)\"\)",
+            # break down a string like 'Toolkit Node: Mono Dpx ("editorial")' into name and output
+            match = re.match("^Toolkit Node: (?P<name>.+) \(\"(?P<output>.+)\"\)",
                 toolkit_specifier)
 
             metadata = match.groupdict()
